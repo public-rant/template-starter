456fa6232a39694de75ac02f5e1f4731a6027613
Richard Anthony <public.rant@pm.me>
2024-10-27 23:48:26 +0000

    wip

--- test/color/test.sh
+++ test/color/test.sh
@@ -5,6 +5,7 @@ source test-utils.sh
 # Template specific tests
 check "distro" lsb_release -c
 check "color" [ $(cat /tmp/color.txt | grep red) ]
+check "act" gh extension install https://github.com/nektos/gh-act
 
 # Report result
 reportResults
