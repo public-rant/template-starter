fb18e778bc91ef82e6e9ce7d753921401a335f9d
Richard Anthony <public.rant@pm.me>
2024-10-28 05:08:56 +0000

    test rclone install

--- test/color/test.sh
+++ test/color/test.sh
@@ -5,6 +5,7 @@ source test-utils.sh
 # Template specific tests
 check "distro" lsb_release -c
 check "color" [ $(cat /tmp/color.txt | grep red) ]
+check "rclone" rclone test selfupdate --dry-run
 
 # Report result
 reportResults
